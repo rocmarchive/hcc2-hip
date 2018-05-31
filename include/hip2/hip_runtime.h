ba6b56180808a1e356be08ffc40efcd0340d5d42
Jan Sjodin <jsjodin@amd.com>
2018-05-31 10:58:02 -0400

    Add device functions to help eigen compilation.

--- include/hip2/hip_runtime.h
+++ include/hip2/hip_runtime.h
@@ -361,6 +361,8 @@ static inline hipError_t hipHostMalloc( T** ptr, size_t size, unsigned int flags
 #include "hip_texture_functions.h"
 #endif
 
+#include "hip_device_functions.h"
+
 #define HIP_LAUNCH_PARAM_BUFFER_POINTER ((void*) 0x01)
 #define HIP_LAUNCH_PARAM_BUFFER_SIZE    ((void*) 0x02)
 #define HIP_LAUNCH_PARAM_END            ((void*) 0x03)
